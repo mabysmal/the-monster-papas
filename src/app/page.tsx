c169bcc51429b3a287fe0d38af834d0824a1098d
Mabel Castillo <maby.garza@gmail.com>
2024-09-13 14:47:34 -0700

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,43 @@
-import Image from "next/image";
-import { Inter, Pirata_One, DM_Serif_Text } from "next/font/google";
+import Image from "next/image";                                                                                                                                       
+import { Inter, Pirata_One, DM_Serif_Text } from "next/font/google";                                                                                                  
 import HamburgerMenu from "@/componentes/hamburger-menu";
-
-
-
-const pirata = Pirata_One ({ weight: ['400'], subsets: ["latin"]})
-const dm = DM_Serif_Text ({ weight:['400'], subsets: ['latin'] })
-const inter = Inter({ subsets: ["latin"] });
-
-
-
-export default function Home() {
-  return (
-    <main className="bg-black ">
-      <HamburgerMenu></HamburgerMenu>
-      <div id="hamb-order" className="flex border-[solid] border-white border-[0.25rem]">
-        <div id="order" className="flex ">
-          <a href="/order" className="a">
-            <button className="text-white border-[solid] border-[#dd9f31]  border-[0.25rem] rounded-lg">
-              Order Here
-            </button>
-          </a>
-        </div>
-        <div id="hamb" className="flex justify-end">
-          <Image
-            className="flex"
-            src="/hamburguesamitadbanner.jpg"
-            width={1080}
-            height={1080}
-            style={{ width: '75%', height: 'auto' }}
-            alt="Mitad de Hamburguesa Clasica"
-          />
-        </div>
-        
-      </div>
-      
-      
-    </main>
-  );
-}
+import './styles.css'                                                                                                             
+                                                                                                                                                                       
+ const pirata = Pirata_One({ weight: ['400'], subsets: ["latin"] });                                                                                                   
+ const dm = DM_Serif_Text({ weight: ['400'], subsets: ['latin'] });                                                                                                    
+ const inter = Inter({ subsets: ["latin"] });                                                                                                                          
+                                                                                                                                                                       
+ export default function Home() {                                                                                                                                      
+   return (                                                                                                                                                            
+     <main className="bg-black">                                                                                                                          
+       <HamburgerMenu />                                                                                                                                               
+       <div id="ordenar" className="flex justify-end items-center py-4">                                                                                               
+         <div className="flex items-center">                                                                                                                           
+           <a href="/order" className="">            
+             <button className={`${dm.className}`}>                                                                                    
+               Order Now!                                                                                                                                              
+             </button>                                                                                                                                                 
+           </a>                                                                                                                                                        
+           <div className="h-full">                                                                                                                                    
+             <Image                                                                                                                                                    
+               src="/hamburguesamitadbanner.jpg"                                                                                                                       
+               width={540}                                                                                                                                             
+               height={540}                                                                                                                                            
+               alt="Mitad de Hamburguesa Clasica"                                                                                                                      
+               className="h-full object-cover"                                                                                                                         
+             />                                                                                                                                                        
+           </div>                                                                                                                                                      
+         </div>                                                                                                                                                        
+       </div>                                                                                                                                                          
+       <div id="catering">                                                                                                                                             
+         <Image                                                                                                                                                        
+           src='/eventos.png'                                                                                                                                           
+           width={1000}                                                                                                                                                
+           height={2000}                                                                                                                                               
+           alt=""                                                                                                                                                      
+           className=""                                                                                                                                                
+         />                                                                                                                                                            
+       </div>                                                                                                                                                          
+     </main>                                                                                                                                                           
+   );                                                                                                                                                                  
+ } 
